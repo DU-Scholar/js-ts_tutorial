2ee8536ddb3a32935d9c2056da2655f87508c617
kazuki-komori <komokazu.nankai@gmail.com>
2021-01-30 18:30:22 +0900

    add: pages

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: "JavaScript, TypeScript 入門講座",
+  description: "JavaScript, TypeScript を入門する方向けへの資料です",
+  themeConfig: {
+    //   头部导航
+    nav: [
+      { text: 'トップ', link: '/' },
+      { text: 'GitHub', link: 'https://github.com/DU-Scholar/js-ts_tutorial' },
+    ],
+    sidebar: [
+      { text: 'JavaScript とは', link: '/pages/0.JavaScriptとは' },
+      { text: 'よく使われる関数', link: '/pages/1.よく使われる関数' },
+      { text: 'TypeScriptとは', link: '/pages/2.TypeScriptとは' },
+      { text: 'よく使われる型', link: '/pages/3.よく使われる型' },
+    ],
+    editLinkText: 'ページに不明点や誤字等があれば、Github にて修正を提案してください！',
+  }
+}
